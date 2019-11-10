d35c1d32792140916eb8182ca652082db208fc2c
Algis <skuodis@gmail.com>
2019-11-10 21:22:51 +0200

    Bubble sort updated, test made

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -4,15 +4,14 @@
 #include <chrono> 
 #include <iterator> 
 #include <omp.h>
+#include <string> 
 
 using namespace std::chrono;
-
 using namespace std;
 
 void PrintArray(int *arr, int size) {
 	for (int i = 0; i < size; i++)
 		cout << arr[i] << " " << flush;
-	cout << endl;
 }
 
 void swap(int *p1, int *p2) {
@@ -21,13 +20,13 @@ void swap(int *p1, int *p2) {
 	*p2 = p;
 }
 
+/* Simple bubble sort */
 void BubbleSort1(int *arr, int n)
 {
 	int swapCount = 1;
 	while (swapCount > 0)
 	{
 		swapCount = 0;
-		// even phase - every even indexed element is compared with next odd indexed element
 		for (int j = 0; j < n - 1; j++)
 			if (arr[j] > arr[j + 1]) {
 				swap(&arr[j], &arr[j + 1]);
@@ -36,6 +35,7 @@ void BubbleSort1(int *arr, int n)
 	}
 }
 
+/* Bubble sort by splitting odd even */
 void BubbleSort2(int *arr, int n)
 {
 	int swapCount = 1;
@@ -60,74 +60,47 @@ void BubbleSort2(int *arr, int n)
 	}
 }
 
-/*void BubbleSortParallel(int *arr, int n)
+/* Same as odd even sort, but parallelizing loops using OpenMP */
+void BubbleSortParallel(int *arr, int n, int num_threads = 8, bool detailedOutput = false)
 {
-	int swapCount = 1;
+	int swapEvenCount = 1;
+	int swapOddCount = 1;
 
-		while (swapCount > 0)
-		{
-			#pragma omp parallel shared(arr)
-			{
-				swapCount = 0;
-
-				// even phase - every even indexed element is compared with next odd indexed element
-				#pragma omp for reduction(+: swapCount)
-				for (int j = 0; j < n - 1; j += 2)
-					if (arr[j] > arr[j + 1]) {
-						swap(&arr[j], &arr[j + 1]);
-						swapCount++;
-					}
-
-				// odd phase - every odd indexed element is compared with next even indexed element
-				#pragma omp for reduction(+: swapCount) 
-				for (int j = 1; j < n - 1; j += 2)
-					if (arr[j] > arr[j + 1]) {
-						swap(&arr[j], &arr[j + 1]);
-						swapCount++;
-					}
-			}
-		}
-}*/
-
-void BubbleSortParallel(int *arr, int n)
-{
-	int swapCount = 1;
-	while(swapCount>0)
+	while (swapEvenCount > 0 || swapOddCount > 0)
 	{
-		swapCount = 0;
-		#pragma omp parallel num_threads(2,100) 
-		{
-			int num = omp_get_thread_num();
-			#pragma omp parallel for reduction(+: swapCount) num_threads(100)  
-			for (int j = num; j < n - 1; j += 2)
-				if (arr[j] > arr[j + 1]) {
-					swap(&arr[j], &arr[j + 1]);
-					swapCount++;
-				}
+		swapEvenCount = swapOddCount = 0;
+
+		omp_set_num_threads(num_threads);
+		#pragma omp parallel for reduction(+: swapEvenCount)  
+		for (int j = 0; j < n-1-n%2; j += 2)
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+				swapEvenCount++;
+			}
+		
+		if (detailedOutput) {
+			PrintArray(arr, n);
+			cout << "Swapped even(0) " << swapEvenCount << endl;
 		}
+
+		omp_set_num_threads(num_threads);
+		#pragma omp parallel for reduction(+: swapOddCount) 
+		for (int j = 1; j < n-1-(1-n%2); j += 2)
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+				swapOddCount++;
+			}
+
+		if (detailedOutput) {
+			PrintArray(arr, n);
+			cout << "Swapped odd(1) " << swapOddCount << endl;
+		}
+
 	}
 }
 
-/*void BubbleSortParallel(int *arr, int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		if (i % 2 == 0) {
-			#pragma omp parallel for shared(arr) num_threads(8)
-			for (int j = 2; j < n; j += 2)
-				if (arr[j - 1] > arr[j])
-					swap(&arr[j - 1], &arr[j]);
-		} else {
-			#pragma omp parallel for shared(arr) num_threads(8)
-			for (int j = 1; j < n; j += 2)
-				if (arr[j - 1] > arr[j])
-					swap(&arr[j - 1], &arr[j]);
-		}
-	}
-}*/
-
-bool DoTest() {
-	const int testSize = 10;
+/* Helper routine to make test */
+bool DoTest(int testSize, bool detailedOutput) {
 	cout << "Performing sorting test of " << testSize << " size " << endl;
 	int * testArray1 = new int[testSize];
 	int * testArray2 = new int[testSize];
@@ -140,19 +113,21 @@ bool DoTest() {
 		testArray2[i] = r;
 		testArray3[i] = r;
 	}
-	PrintArray(testArray1, testSize);
-	cout << "Serial sort of " << testSize << " size " << endl;
+	PrintArray(testArray1, testSize); cout << endl;
+
 	BubbleSort1(testArray1, testSize);
-	PrintArray(testArray1, testSize);
-	cout << "Odd even sort of " << testSize << " size " << endl;
-	BubbleSort1(testArray2, testSize);
-	PrintArray(testArray2, testSize);
-	cout << "Parallel sort of " << testSize << " size " << endl;
-	BubbleSortParallel(testArray3, testSize);
-	PrintArray(testArray3, testSize);
+	BubbleSort2(testArray2, testSize);
+
+	BubbleSortParallel(testArray3, testSize, detailedOutput);
+
+	if (detailedOutput) {
+		PrintArray(testArray3, testSize); 
+		cout << endl;
+	}
+
 	bool testPass = true;
 	for (int i = 0; i < testSize; i++) {
-		if (testArray1[i] != testArray2[i]) {
+		if (!(testArray1[i] == testArray3[i] )) {
 			testPass = false;
 			break;
 		}
@@ -166,25 +141,8 @@ bool DoTest() {
 	return testPass;
 }
 
-
-int main(int argc, char *argv[])
-{
-	bool doTest = true;
-
-	/*if (argc < 2) {
-		cout << "bubblesort test - performs sorting test" << endl;
-		cout << "bubblesort benchmark - performs benchmark from 1000 to 50000 stepping by 5000 " << endl;
-		cout << "bubblesort single [arraysize] [cores] - performs single arrayt sort on [arraysize] using number of [cores] " << endl;
-		return(0);
-	} else {
-		if (argv[1]==)
-	}*/
-
-	if (doTest) {
-		DoTest();
-		//return(0);
-	}
-
+/* Do benchmark using provided number of threads */
+void DoBenchamrk(int numThreads) {
 	for (int size = 1000; size <= 100000; size += 5000) {
 
 		int * array1 = new int[size];
@@ -209,18 +167,44 @@ int main(int argc, char *argv[])
 		auto durationSerial2 = duration_cast<milliseconds>(stop2 - start2);
 
 		auto start3 = high_resolution_clock::now();
-		BubbleSortParallel(array3, size);
+		BubbleSortParallel(array3, size, numThreads);
 		auto stop3 = high_resolution_clock::now();
 		auto durationParallel = duration_cast<milliseconds>(stop3 - start3);
-		
-		cout << size << 
-			";" << durationSerial.count() << 
+
+		cout << size << ";" << numThreads << ";" <<
+			";" << durationSerial.count() <<
 			";" << durationSerial2.count() <<
 			";" << durationParallel.count() << endl;
 
 		delete array1;
 		delete array2;
-
 	}
+}
+
+int main(int argc, char *argv[])
+{
+
+	if (argc < 2) {
+		cout << "Naudojimas:" << endl;
+		cout << "BUBBLESORT test [size] [detailed_output] - perform sorting test, check if it sorts correctly." << endl;
+		cout << "BUBBLESORT benchmark [num_threads] - performs benchmark from 1000 to 100000 stepping by 5000 " << endl;
+		return(0);
+	} else {
+		if (string(argv[1]) == "test") {
+			int size = stoi(argv[2]);
+			bool detailed = argv[3] == "true";
+			DoTest(size,detailed);
+			return (0);
+		}
+		else if (string(argv[1]) == "benchmark") {
+			int numThreads = stoi(argv[2]);
+			DoBenchamrk(numThreads);
+			return (0);
+		}
+		else {
+			cout << "Unknown argument " << argv[1] << endl;
+			return (0);
+		}
+	}	
 }
 
